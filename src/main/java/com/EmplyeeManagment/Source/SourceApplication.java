af8c998ae28d35d8ef78717e1abe4c41da68a665
RON_ALLOGO <ronallogo45@gmail.com>
2024-07-31 15:23:32 +0200

    first commit

---
+++ src/main/java/com/EmplyeeManagment/Source/SourceApplication.java
@@ -0,0 +1,13 @@
+package com.EmplyeeManagment.Source;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EmplyeeManagment/Source/SourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EmplyeeManagment.Source;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
