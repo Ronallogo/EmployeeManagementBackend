4821b56367b7227b60e1324f571e69ed0f4b9deb
RON_ALLOGO <ronallogo45@gmail.com>
2024-09-07 16:21:39 +0200

    commit for 07/09/2024

--- src/main/java/com/EmployeeManagment/Source/SourceApplication.java
+++ src/main/java/com/EmployeeManagment/Source/SourceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SourceApplication {
-
+/**/
 	public static void main(String[] args) {
 		SpringApplication.run(SourceApplication.class, args);
 	}
